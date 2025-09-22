34fad6fb9cbbe02a66765f7143da3f78fe82107c
GreensandGrains <greensandgrains2025@gmail.com>
2025-09-22 22:07:18 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express, Request } from "express";
 import { createServer, type Server } from "http";
-import { storage } from "./storage";
-import { db } from "./db";
+import { storage } from "@server/storage";
+import { db } from "@server/db";
 import {
   servers,
   bots,
