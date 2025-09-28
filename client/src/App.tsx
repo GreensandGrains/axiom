ee2a34e23fd7001b53a3acc942a8a48f46e82885
GreensandGrains <greensandgrains2025@gmail.com>
2025-09-28 14:28:16 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -42,7 +42,6 @@ import Payment from "@/pages/payment";
 import PaymentSuccess from "@/pages/payment-success";
 import YourBots from "@/pages/your-bots";
 import BotDetail from "@/pages/bot-detail";
-import google9fd896c9fede5b13 from "@/pages/google9fd896c9fede5b13";
 import LoadingPage from "@/components/loading-page";
 import OfflinePage from "@/components/offline-page";
 import { useAppState } from "@/hooks/use-app-state";
@@ -71,7 +70,6 @@ function Router() {
       <Route path="/jobs" component={Jobs} />
       <Route path="/profile" component={Profile} />
       <Route path="/admin" component={AdminPage} />
-      <Route path"/google" component={google9fd896c9fede5b13} />
       <Route path="/search" component={SearchPage} />
       <Route path="/server/:id" component={ServerDetail} />
       <Route path="/help" component={Help} />
